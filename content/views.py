220eb6553e044dc63e4c51395ad2fadc6adecadc
gnunoo <qhsdh7321@gmail.com>
2022-08-18 15:45:01 +0900

    프로필 페이지에 피드 띄우기(내가 올린 피드,좋아요,북마크)

--- content/views.py
+++ content/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 
+from instagram.models import Feed, Like, Bookmark
 from user.models import User
 
 
@@ -17,4 +18,10 @@ class Profile(APIView):
         if user is None:
             return render(request, "user/login.html")
 
-        return render(request, "content/profile.html", context=dict(user=user))
+        feed_list=Feed.objects.filter(email=email).all()
+        like_list=list(Like.objects.filter(email=email,is_like=True).values_list('feed_id',flat=True))
+        like_feed_list=Feed.objects.filter(id__in=like_list)
+        bookmark_list=list(Bookmark.objects.filter(email=email,is_marked=True).values_list('feed_id',flat=True))
+        bookmark_feed_list=Feed.objects.filter(id__in=bookmark_list)
+
+        return render(request, "content/profile.html", context=dict(feed_list=feed_list, like_feed_list=like_feed_list, bookmark_feed_list=bookmark_feed_list , user=user))
